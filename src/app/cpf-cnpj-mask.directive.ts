42858e394f9f25acd534936677e7baea481de006
efraibo <efraibo@gmail.com>
2019-10-25 15:39:38 -0300

    removendo códigos desnecessários

--- src/app/cpf-cnpj-mask.directive.ts
+++ src/app/cpf-cnpj-mask.directive.ts
@@ -41,20 +41,6 @@ export class MaskDirective {
     } else if (newVal.length < 15) {
       newVal = newVal.replace(/^(\d{0,2})(\d{0,3})(\d{0,3})(\d{0,4})(\d{0,2})$/, '$1.$2.$3/$4-$5');
     }
-
-
-    //if (newVal.length === 0) {
-    //  newVal = '';
-    //} else if (newVal.length <= 3) {
-    //  newVal = newVal.replace(/^(\d{0,3})/, '($1)');
-    //} else if (newVal.length <= 6) {
-    //  newVal = newVal.replace(/^(\d{0,3})(\d{0,3})/, '($1) ($2)');
-    //} else if (newVal.length <= 10) {
-    //  newVal = newVal.replace(/^(\d{0,3})(\d{0,3})(\d{0,4})/, '($1) ($2)-$3');
-    //} else {
-    //  newVal = newVal.substring(0, 10);
-    //  newVal = newVal.replace(/^(\d{0,3})(\d{0,3})(\d{0,4})/, '($1) ($2)-$3');
-   // }
     this.ngControl.valueAccessor.writeValue(newVal);
   }
 }
